928e106710430b1dd1b512d8198c9374cb008571
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2013-05-04 06:51:24 +0800

    [tmux] add tmux.conf

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ ln -sf $PWD/gitconfig $HOME/.gitconfig
 ln -sf $PWD/gitignore_global $HOME/.gitignore_global
 ln -sf $PWD/vim $HOME/.vim
 ln -sf $PWD/vimrc $HOME/.vimrc
+ln -sf $PWD/tmux.conf $HOME/.tmux.conf
